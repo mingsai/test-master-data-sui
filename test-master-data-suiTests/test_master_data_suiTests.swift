0354cfea02d1e75e3838643ee8ed213362541af8
Tommie N. Carter, Jr <tcarternyc@gmail.com>
2019-09-02 02:20:29 -0400

    Initial Commit

---
+++ test-master-data-suiTests/test_master_data_suiTests.swift
@@ -0,0 +1,34 @@
+//
+//  test_master_data_suiTests.swift
+//  test-master-data-suiTests
+//
+//  Created by Tommie Carter on 9/2/19.
+//  Copyright © 2019 MING Technology LLC. All rights reserved.
+//
+
+import XCTest
+@testable import test_master_data_sui
+
+class test_master_data_suiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
